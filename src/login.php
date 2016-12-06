5b3c46a1e9fd843483683fcbb2962464b170c284
mehdi <mehdi.mkadem@yahoo.com>
2016-12-06 17:19:34 +0100

    comment

--- src/login.php
+++ src/login.php
@@ -1,14 +1,29 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-<html>
-<head>
-<meta charset="utf-8" />
-<title>Authentification</title>
-</head>
-<body>
+require_once('../lib/user-data.lib.php');
+require_once('../lib/checksession.lib.php');
+
+
+$login = $_POST["login"];
+$password = $_POST["password"];
+$error_msg = "";
+if (checkUser($login, $password)) {
+
+
+    createSession();
+
+    header("Location: /src/showPlanning.php");
+} else {
+
+    $error_msg = 'login incorrect';
+}
+
+
+
+require('../html/login.html.php');
