9b3f492daaf31bc49ee9f2820b744aeccdd59a7a
Dihcar <edjek@hotmail.fr>
2016-12-07 11:05:57 +0100

    sub 1.6

--- src/subscribe.php
+++ src/subscribe.php
@@ -1,26 +1,16 @@
 <?php
 
+require_once '../lib/user-data.lib.php';
+
 $login = $_POST['login'];
 $password = $_POST['password'];
 $name = $_POST['name'];
+$error = FALSE;
 
 //je vérifie que toutes les info du formulaire sont là
-if (!empty($login) && !empty($password) && !empty($name)){
-         
-    if (!checkUser($login, $passwordw))  
-    {
-        createUser($login, $passwordw, $name);
-        header('Location: ../html/login.html.php');
-    }
-    else    {
-        echo "<div>" .
-            "<p>Ce compte existe déjà</p>".
-            "<a href='subscribe.php'><p class='erreur'>Page précédente<p></a>" .
-        "</div>"; }
+if (!empty($login) && !empty($password) && !empty($name)) {
+
+
+    createUser($login, $password, $name);
+    echo 'good';
 }
-else    {
-    echo
-        '<h2 class="title">Vous n\'avez pas rempli tous les champs!</h2>' .
-        '<a href="../html/subscribe.html.php"><p class="erreur">Page precedente<p></a>' .
-    '</div>'; }
-?>
