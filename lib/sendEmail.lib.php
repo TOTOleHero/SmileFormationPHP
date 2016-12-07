5cf3167abe833a70a022e1a12da0a12a7f56a47e
Thomas Lecarpentier <thomas.lecarpentier@smile.fr>
2016-12-07 16:34:51 +0100

    check bug

--- lib/sendEmail.lib.php
+++ lib/sendEmail.lib.php
@@ -1,17 +1,36 @@
 <?php
 
+define('EMAIL_SERVICE_URL','http://localhost:8888/sendEmail');
 
-$data_string
-
-    
-
-
-
-$ch = curl_init('http://localhost:8888/sendEmail');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER,[
-                    "Content-Type : application/json"
+/**
+ *
+ * send fake email
+ *
+ * @param array $dataEmail
+ * [
+        "firstName" => "firstName",
+        "lastName"  => "lastName",
+        "email"     => "firstName.lastName@domain.fr",
+        "subject"   => "subject",
+        "body"      => "body"
+    ]
+ * @return boolean
+ */
+function sendEmail($dataEmail)
+{
+    $ch     = curl_init(EMAIL_SERVICE_URL);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $dataEmail);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Content-Type : application/json"
     ]);
-$result = curl_exec($ch);
+    $result = curl_exec($ch);
+
+
+    if ($result == "send-ok") {
+        return true;
+    }
+
+    return false;
+}
