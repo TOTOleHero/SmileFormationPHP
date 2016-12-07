9398504e41f760080fd3dd38859d69a06c500f94
formation <labehri2000@yahoo.fr>
2016-12-07 11:00:24 +0100

    comment

--- lib/checksession.lib.php
+++ lib/checksession.lib.php
@@ -9,12 +9,24 @@ function createSession(){
     return true;
 }
 
+function destroySession(){
+
+    $_SESSION = array();
+    
+    session_destroy();
+    
+    $_SESSION["connected"]=false;
+
+    return false;
+}
+
 function isConnected(){
+    
+    session_start();
+
     if(isset($_SESSION["connected"]) && $_SESSION["connected"]==true){
             return true;
     }
 
-    else{ 
-        return false;
-    }
+   
 }
