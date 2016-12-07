af8e13cc649d425c5767bfee2e5b3564e0bfc6ad
Dihcar <edjek@hotmail.fr>
2016-12-07 11:41:38 +0100

    sub 1.8

--- src/subscribe.php
+++ src/subscribe.php
@@ -1,17 +1,32 @@
 <?php
-include '../html/subscribe.html.php';
+
 require_once '../lib/user-data.lib.php';
+include '../html/subscribe.html.php';
 
-$login = isset($_POST['login']);
-$password = $_POST['password'];
-$name = $_POST['name'];
-$error = FALSE;
+if(isset($_POST['login']) && isset($_POST['password']) && isset($_POST['name'])){
+    
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $name = $_POST['name'];
+    
+    //je vérifie que toutes les info du formulaire sont là
+    if (!empty($login) && !empty($password) && !empty($name)) {
 
-//je vérifie que toutes les info du formulaire sont là
-if (!empty($login) && !empty($password) && !empty($name)) {
 
+        if (createUser($login, $password, $name)){
+            echo "le compte a été créé.";
+            ?> <a href="login.php" >Se connecter</a><?php
+        }
+        else{
+            echo "l'utilisateur existe déjà";
+        }
+        
 
-    createUser($login, $password, $name);
-    header("Location: login.php");
-
+    }
+    else{
+        echo 'Tous vos champs ne sont pas remplis';
+    }
+}
+else{
+    echo'Veuillez remplir le formulaire';
 }
