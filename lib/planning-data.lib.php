287ac1e70c5333b80aebb13e8d96187a561e2f6c
mehdi <mehdi.mkadem@yahoo.com>
2016-12-07 17:03:46 +0100

    comment

--- lib/planning-data.lib.php
+++ lib/planning-data.lib.php
@@ -1,10 +1,10 @@
 <?php
 
-
-/*Declaration of constants*/
+/* Declaration of constants */
 
 // Source data file name
-define('SOURCE_CSV_FILE', '../data/formation-planning.csv');
+define('SOURCE_CSV_FILE', __DIR__. '/../data/formation-planning.csv');
+define('SOURCE_PHP_FILE', __DIR__. '/../data/formation-planning.serialized-php.data');
 
 // Start of data
 define('DATE_INDEX', 0);
@@ -13,59 +13,75 @@ define('LABEL_INDEX', 1);
 /*
  * Function that returns an array containing dates in YYYYMMDD format and course name
  */
+
 function getData() {
-    $outputData = [];
+    return loadPlanningData();
+}
 
+function getDataAtDate($date = null) {
 
-    // Load and read each line of the CSV file
-    // Build the data table
-    if (($handle = fopen(SOURCE_CSV_FILE, "r")) !== FALSE) {
-        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            
-            if ($data[DATE_INDEX] == 'date') {
-                continue;
-            }
-      
-            $outputData[] = [
-                        'date' => $data[DATE_INDEX],
-                        'label' => $data[LABEL_INDEX]
-            ];
-        }
-
-            // References data table
-            return $outputData;
-        }
-} 
-
-
-
-function getDataAtDate($date = null){
-    
-    if ($date == null){
+    if ($date == null) {
         $date = date("Ymd");
-    }elseif (preg_match('/^20\d{2}[01]\d[0123]\d$/', $date) === 0) {
-        echo 'Date not valid, format must be YYYYMMDD';        
+    } elseif (preg_match('/^20\d{2}[01]\d[0123]\d$/', $date) === 0) {
+        echo 'Date not valid, format must be YYYYMMDD';
     }
-    
+
     $data = getData();
 
-    
+
     foreach ($data as $value) {
-        if ($value['date'] == $date){
+        if ($value['date'] == $date) {
             return $value['label'];
         }
     }
-     
+}
+
+function createPLanning($date, $cours, $nameTeacher) {
+
+    
+    $data = loadPlanningData();
+    
+    // TEST SI EXIST DEJA
+    
+    
+    $data[] = [
+        "date"=>$date,
+        "label"=>$cours,
+        "teacher"=>$nameTeacher
+    ];
+    
+    persistPlanningData($data);
+
+    return True;
+}
+
+function updatePLanning($date, $cours, $nameFormater) {
     
 }
 
-function createPLanning(){
+function deletePLanning($date) {
     
 }
 
-function updatePLanning(){
-    
+/**
+ * persist PHP data on disk
+ * @param array $data
+ */
+function persistPlanningData(array $data) {
+    file_put_contents(SOURCE_PHP_FILE, serialize($data));
 }
-function deletePLanning(){
-    
+
+
+/**
+ * laod PHP data from disk
+
+ * @return array
+ */
+function loadPlanningData() {
+    $rawData = file_get_contents(SOURCE_PHP_FILE);
+
+    $data = unserialize($rawData);
+
+    return $data;
 }
+
