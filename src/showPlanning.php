7e0d83b7311c19ac031a30ab31595f5c323e1e4b
Mar_Des <deschamps.ma@gmail.com>
2016-12-08 09:26:43 +0100

    new version

--- src/showPlanning.php
+++ src/showPlanning.php
@@ -6,5 +6,23 @@ if (isConnected() === FALSE) {
     header("Location: login.php");
 }
 
+$admin=TRUE;
+
+
 $outputData = getData();
 include('../html/showPlanning.html.php');
+//
+//if (isset($_POST["Date"] ) && isset($_POST["Label"] ) && isset($_POST["Formateur"] )){
+//    $date=$_POST["Date"];
+//    $label=$_POST["Label"];
+//    $formateur=$_POST["Formateur"]; 
+//    
+//    
+//    createPlanning($date, $label, $formateur);
+//    
+//}
+
+
+
+updatePLanning($_POST('lineData'));
+deletePLanning($_POST('lineData'));
