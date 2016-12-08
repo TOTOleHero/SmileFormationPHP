c11543b245ef7791467e469cd4d162f3cf53e5b3
row23 <romaric.gueye@gmail.com>
2016-12-08 10:13:41 +0100

    merge some bug

--- lib/sendEmail.lib.php
+++ lib/sendEmail.lib.php
@@ -1,35 +1,23 @@
 <?php
 
-define('EMAIL_SERVICE_URL','http://localhost:8888/sendEmail');
+define('EMAIL_SERVICE_URL', 'http://localhost:8888/sendEmail');
 
-<<<<<<< HEAD
-$data = [
-    	"firstName"=> "firstName",
-	"lastName"=> "lastName",
-	"email"=> "firstName.lastName",
-	"subject"=> "subject",
-	"body"=> "body"
-];
-
-//$data_string =]son_encode($data);
-=======
 /**
  *
  * send fake email
  *
  * @param array $dataEmail
  * [
-        "firstName" => "firstName",
-        "lastName"  => "lastName",
-        "email"     => "firstName.lastName@domain.fr",
-        "subject"   => "subject",
-        "body"      => "body"
-    ]
+  "firstName" => "firstName",
+  "lastName"  => "lastName",
+  "email"     => "firstName.lastName@domain.fr",
+  "subject"   => "subject",
+  "body"      => "body"
+  ]
  * @return boolean
  */
-function sendEmail($dataEmail)
-{
-    $ch     = curl_init(EMAIL_SERVICE_URL);
+function sendEmail($dataEmail) {
+    $ch = curl_init(EMAIL_SERVICE_URL);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $dataEmail);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -42,7 +30,5 @@ function sendEmail($dataEmail)
     if ($result == "send-ok") {
         return true;
     }
->>>>>>> 93468dd0ef3faafc92987c1a01689ab07cfea1da
-
     return false;
 }
