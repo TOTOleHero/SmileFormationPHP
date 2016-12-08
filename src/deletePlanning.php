62b71962d3f3f6449f1b7b7004b8f0d859da05f6
Mar_Des <deschamps.ma@gmail.com>
2016-12-08 15:41:06 +0100

    correctif

--- src/deletePlanning.php
+++ src/deletePlanning.php
@@ -2,34 +2,25 @@
 
 require '../lib/planning-data.lib.php';
 
-
-
-//echo $_POST['lineDate'];
-
-
 $lineDate = "";
 $label = "";
 $teacher = "";
 
-
- if (isset($_POST["lineDate"])){
+if (isset($_POST["lineDate"])) {
     $lineDate = $_POST["lineDate"];
 
     $infos = getDataAtDate($lineDate);
-    if($infos!== null)
-    {
-        
-    $label = $infos['label'];
-    $teacher = $infos['teacher'];
+    if ($infos !== null) {
+
+        $label = $infos['label'];
+        $teacher = $infos['teacher'];
     }
- }    
- 
- 
+}
+
 if (isset($_POST["confirmDelete"])) {
 
-
     if (isset($_POST["lineDate"]) && isset($_POST["label"]) && isset($_POST["teacher"])) {
-        $lineDate= $_POST["lineDate"];
+        $lineDate = $_POST["lineDate"];
         $label = $_POST["label"];
         $teacher = $_POST["teacher"];
         deletePLanning($lineDate);
